bed8ada4c4f9e62f3562c061978f74ebed990949
Munipapireddygari Dilip Kumar Reddy <dilipkumarreddy1412@gmail.com>
2026-04-17 20:46:17 +0530

    test

---
+++ test6.py
@@ -0,0 +1,112 @@
+daynum=int(input("enter the number:"))
+if(daynum ==0):
+           print("sunday")
+elif(daynum ==1):
+           print("monday")
+elif(daynum ==2):
+           print("tuesday")
+elif(daynum ==3):
+           print("wednesday")
+elif(daynum ==4):
+           print("thursday")
+elif(daynum ==5):
+           print("friday")
+elif(daynum ==6):
+           print("saturday")
+else:
+    print("invalid number! try again")
+
+
+
+
+def printDay(daynum):
+    if daynum ==0:
+        return "sunday"
+    elif daynum ==1:
+        return "monday"
+    elif daynum ==2:
+        return "tuesday"
+    elif daynum ==3:
+        return "wednesday"
+    elif daynum ==4:
+        return "thursday"
+    elif daynum ==5:
+        return "friday"
+    elif daynum ==6:
+        return "saturday"
+    else:
+        return"invalid"
+dn=int(input("enter day number:"))
+print(printDay(dn))
+
+
+
+
+
+
+
+
+
+
+def printDay(daynum):
+    if daynum ==0:
+        return "jan"
+    elif daynum ==1:
+        return "feb"
+    elif daynum ==2:
+        return "mar"
+    elif daynum ==3:
+        return "april"
+    elif daynum ==4:
+        return "may"
+    elif daynum ==5:
+        return "jun"
+    elif daynum ==6:
+        return "jul"
+    elif daynum ==6:
+        return "aug"
+    elif daynum ==6:
+        return "sep"
+    elif daynum ==6:
+        return "oct"
+    elif daynum ==6:
+        return "nov"
+    elif daynum ==6:
+        return "dec"
+    else:
+        return"invalid"
+dn=int(input("enter month number:"))
+print(printDay(dn))
+
+
+
+
+
+
+
+
+
+
+
+
+def printDay(daynum):
+    if daynum ==0:
+        return "sorry your choice bad because is likith"
+    elif daynum ==1:
+        return "sorry your choice bad because is venky"
+    elif daynum ==2:
+        return "your choice is better dilip"
+    elif daynum ==3:
+        return "sorry your choice is bad because nagesh"
+    elif daynum ==4:
+        return "sorry your choice is bad because eswar"
+    elif daynum ==5:
+        return "sorry your choice is bad because prasad"
+    elif daynum ==6:
+        return "sorry your choice is bad because mahesh"
+    else:
+        return"invalid"
+dn=int(input("enter your best friend number:"))
+print(printDay(dn))
+    
+    
