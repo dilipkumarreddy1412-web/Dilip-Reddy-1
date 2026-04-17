b9b08163cf5adf649288a8e980d51ea5bd24d8a5
Munipapireddygari Dilip Kumar Reddy <dilipkumarreddy1412@gmail.com>
2026-04-17 20:46:06 +0530

    test

---
+++ test5.py
@@ -0,0 +1,60 @@
+num=int(input("enter the number:"))
+if (num %2==0):
+    print(num,"is even number")
+else:
+    print(num,"is odd number")
+
+
+    
+
+
+num+int(input("enter a number:"))
+if (num %2==0):
+    print(num,"is even number")
+    print("first if block will be executed")
+
+if (num %2==0):
+    print("number is odd number")
+    print("second if block will be executed")
+
+
+    
+
+num=int(input("enter the value"))
+if(num %2==0):
+    print("number is even")
+    print("if block executed")
+
+else:
+    print("the number is odd")
+    print("else block executed")
+    print("thank you")
+
+
+    
+
+num=input("Enter a number")
+if num[-1]in'02468':
+    print(num,"is even number")
+else:
+    print(num,"is odd")
+
+
+    
+
+
+def sumDigits(n):
+    sd=0
+    for i in n:
+        sd=sd+int(i)
+        return sd
+inps=input("enter the value:")
+print(sumDigits(inps))
+
+    
+    
+
+
+    
+
+    
