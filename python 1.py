25353304c1eb62122851548fbad113aa3c6f7c44
Munipapireddygari Dilip Kumar Reddy <dilipkumarreddy1412@gmail.com>
2026-04-17 20:42:10 +0530

    python

---
+++ python 1.py
@@ -0,0 +1,151 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import builtins
+dir(builtins)
+['ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException', 'BaseExceptionGroup', 'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning', 'ChildProcessError', 'ConnectionAbortedError', 'ConnectionError', 'ConnectionRefusedError', 'ConnectionResetError', 'DeprecationWarning', 'EOFError', 'Ellipsis', 'EncodingWarning', 'EnvironmentError', 'Exception', 'ExceptionGroup', 'False', 'FileExistsError', 'FileNotFoundError', 'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning', 'IndentationError', 'IndexError', 'InterruptedError', 'IsADirectoryError', 'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError', 'ModuleNotFoundError', 'NameError', 'None', 'NotADirectoryError', 'NotImplemented', 'NotImplementedError', 'OSError', 'OverflowError', 'PendingDeprecationWarning', 'PermissionError', 'ProcessLookupError', 'RecursionError', 'ReferenceError', 'ResourceWarning', 'RuntimeError', 'RuntimeWarning', 'StopAsyncIteration', 'StopIteration', 'SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError', 'TimeoutError', 'True', 'TypeError', 'UnboundLocalError', 'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'WindowsError', 'ZeroDivisionError', '__build_class__', '__debug__', '__doc__', '__import__', '__loader__', '__name__', '__package__', '__spec__', 'abs', 'aiter', 'all', 'anext', 'any', 'ascii', 'bin', 'bool', 'breakpoint', 'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'exit', 'filter', 'float', 'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'license', 'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip']
+a=10
+id(a)
+140715675601624
+b=20
+id(b)
+140715675601944
+c=a+b
+c
+30
+id(c)
+140715675602264
+n=20
+m=30
+n+m
+50
+n-m
+-10
+n*m
+600
+n/m
+0.6666666666666666
+n%m
+20
+100-50
+50
+1500+100
+1600
+c=int(101)
+c
+101
+a=int('101',2)
+a
+5
+m=int('134',3)
+
+w=int(101,3)
+
+'twenty'+'five'
+'twentyfive'
+2**3
+8
+2**3**4
+2417851639229258349412352
+2**(1/2)
+1.4142135623730951
+'dilipreddy'+3
+
+'dilipreddy'*3
+'dilipreddydilipreddydilipreddy'
+3*'dilip'
+'dilipdilipdilip'
+q=dilip
+
+q='dilip'
+q
+'dilip'
+man='dilip'
+man
+'dilip'
+2**(1/3)
+1.2599210498948732
+2**(man='dilip')
+
+3**(1/20)
+1.056467308549538
+'dilipreddy'*'dilip'
+
+mahesh='mahesh'
+mahesh
+'mahesh'
+eswar='lover boy'
+eswar
+'lover boy'
+a=int()
+a
+0
+b=int('101',2)
+b
+5
+c=int(10)
+c
+10
+x=int('101',3)
+x
+10
+f=int('789',10)
+f
+789
+g=int('2345')
+g
+2345
+u='mango'
+u
+'mango'
+eswar='magapathi'
+eswar
+'magapathi'
+nagesh='mentel boy'
+nagesh
+'mentel boy'
+bin(5)
+'0b101'
+oct(5)
+'0o5'
+hex(5)
+'0x5'
+bin(5)[2:1]
+''
+bin(5)[2:]
+'101'
+m=123
+n=456
+n+n
+912
+n+m
+579
+m%n
+123
+>>> n/m
+3.707317073170732
+>>> m/m
+1.0
+>>> n/*m
+
+>>> m-n
+-333
+>>> m>n
+False
+>>> m<n
+True
+>>> m>=n
+False
+>>> m<=n
+True
+>>> n<=m
+False
+>>> n>=m
+True
+>>> m!=n
+True
+>>> n!<=m
+SyntaxError: invalid syntax
+>>> n!=m
+True
+>>> m!=n
+True
