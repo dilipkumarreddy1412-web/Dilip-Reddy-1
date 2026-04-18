9240871a5a63a8ada3c2651b178c645617c90170
Munipapireddygari Dilip Kumar Reddy <dilipkumarreddy1412@gmail.com>
2026-04-18 06:13:56 +0530

    Dog

---
+++ Dog.py
@@ -0,0 +1,16 @@
+class Dog:
+    price=400
+    def __init__(self,name,color):
+        self.color=color
+        self.name=name
+    def bark(self):
+        print("Woof")
+        print(self.name,"has",self.price,"price and its color is",self.color)
+pet1=Dog("tommy","brown")
+pet2=Dog("sheru","white")
+pet1.bark()
+pet2.bark()
+print(pet1.price)
+print(pet2.price)
+print(Dog.price)
+Dog('abc','blue').bark()
